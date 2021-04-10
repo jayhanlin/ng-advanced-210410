e2c4ee0934b1419a8b5339dafdbdefceeb13ba7c
ed.lin <ed.lin@esunergy.com>
2021-04-10 13:11:41 +0800

    練習路由參數如何傳遞

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,8 @@ const routes: Routes = [
   {
     path: 'utilities',
     children: [
-      { path: 'color', component: ColorComponent }
+      { path: 'color', component: ColorComponent },
+      { path: 'color/:type', component: ColorComponent },//:type必要參數，如沒給此參數會進404
     ]
   },
   { path: '**', component: NotFoundComponent }, //萬用路由，任何找不到Path都會進來此Component，且要放在最後，放上面時，會比對到，整個網站都會進NotFoundComponent
--- src/app/utilities/color/color.component.ts
+++ src/app/utilities/color/color.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './color.component.html',
@@ -6,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorComponent implements OnInit {
 
-  constructor() { }
+  type = 0;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    // 寫法1
+    this.type = +this.route.snapshot.paramMap.get('type');
+
+    // 寫法2
+    this.route.paramMap.subscribe(params => {
+      this.type = +params.get('type');
+    });
+
+    // 寫法3  舊寫法，不建議用此寫法，因為弱型別
+    // this.type = this.route.snapshot.params['type'];
+
+    // 寫法4  舊寫法，不建議用此寫法，因為弱型別
+    // this.route.params.subscribe(params => {
+    //   this.type = params.get('type');
+    // });
   }
 
+  plusOne() {
+    this.router.navigate(['/utilities/color/', this.type + 1])
+  }
 }
