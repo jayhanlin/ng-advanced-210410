ef653b263358ebaf09d7bc45dc980e09fab0c3e0
ed.lin <ed.lin@esunergy.com>
2021-04-10 11:34:51 +0800

    練習子路由設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { ColorComponent } from './utilities/color/color.component';
 
 const routes: Routes = [
   // { path: '', component: DashboardComponent },  // 首頁直接導去DashboardComponent 寫法1
@@ -11,6 +12,12 @@ const routes: Routes = [
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
   { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'utilities',
+    children: [
+      { path: 'color', component: ColorComponent }
+    ]
+  },
   { path: '**', component: NotFoundComponent }, //萬用路由，任何找不到Path都會進來此Component，且要放在最後，放上面時，會比對到，整個網站都會進NotFoundComponent
 ];//在此新增路由規則
 
